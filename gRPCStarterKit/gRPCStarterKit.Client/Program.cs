bfd38de85fff249f83677d1359a787d06bebf296
Thomas Blondel <tfoenix@gmail.com>
2023-05-10 13:37:37 +0200

    [Client] Added basic code to test gRPC service

--- gRPCStarterKit/gRPCStarterKit.Client/Program.cs
+++ gRPCStarterKit/gRPCStarterKit.Client/Program.cs
@@ -1,3 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using Grpc.Net.Client;
+using gRPCStarterKit.Client;
+
+var channel = GrpcChannel.ForAddress("http://localhost:5238");
+var client = new Greeter.GreeterClient(channel);
+
+var reply = await client.SayHelloAsync(
+    new HelloRequest { Name = "Thomas" });
+Console.WriteLine("Greeting: " + reply.Message);
